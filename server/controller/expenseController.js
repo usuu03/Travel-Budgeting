36af38418f12724d5e35f4f90981c5e5c1a8ac85
Usu <usuedeaghe@gmail.com>
2023-10-11 21:19:56 +0100

    setting up expense controller

--- server/controller/expenseController.js
+++ server/controller/expenseController.js
@@ -1,1 +1,19 @@
-const Expense = require;
+const Expense = require("../models/Expense");
+
+const getAllExpenses = (req, res) => {};
+
+const getExpenseById = (req, res) => {};
+
+const addExpense = (req, res) => {};
+
+const updateExpense = (req, res) => {};
+
+const deleteExpense = (req, res) => {};
+
+module.exports = {
+  getAllExpenses,
+  getExpenseById,
+  addExpense,
+  updateExpense,
+  deleteExpense,
+};
